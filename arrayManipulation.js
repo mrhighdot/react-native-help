1ba7b29422ca29d35e923e964c178b6e35fc174e
Gabriel Omane - Yeboah <gabrielomane@outlook.com>
2024-05-17 17:32:32 +0000

    Task 2 completed

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,13 +1,37 @@
+//Task 1
 //Declaring array of numbers
-let numArray = [1, 2, 3, 4]
+const numArray = [7, 2, 3, 4];
 
+//Array manipulation function which doubles even numbers and triples odd numbers
 function processArray(array) {
-	const newArray = array.map(number => {
-		if (number % 2 === 0) { return Math.pow(number, 2) } else if (number % 2 === 1) { return Math.pow(number, 3) }
-	})
-	console.log(newArray)
-	return newArray
+  //Loops over the array to perform the power operation
+  const newArray = array.map((number) => {
+    if (number % 2 === 0) return Math.pow(number, 2);
+    else if (number % 2 === 1) return Math.pow(number, 3);
+  });
+  //Output to the console
+  console.log(newArray);
+  //Returns the new array
+  return newArray;
 }
 
-processArray(numArray)
+//Run the function
+const processedArray = processArray(numArray);
 
+//Task 2
+//Create fromatArrayStrings function
+function formatArrayStrings(strings, nums) {
+  const newNumMap = nums.map((num, i) => {
+    if (num % 2 === 0) {
+      const alterString = strings[i].toUpperCase();
+      return alterString;
+    } else if (num % 2 === 1) {
+      const alterString = strings[i].toLowerCase();
+      return alterString;
+    }
+  });
+  console.log(newNumMap);
+  return newNumMap;
+}
+
+formatArrayStrings(["test", "pink", "blue", "we"], processedArray);
